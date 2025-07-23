835b3463e7854b9469521d734760b02ac7152d5e
Jiahao Huang <j.huang21@imperial.ac.uk>
2025-07-24 00:07:03 +0800

    24/Jul/2025 update

--- nnUNetCls/nnunetv2/training/nnUNetTrainer/variants/cls/nnUNetClsTrainerDEBUG.py
+++ nnUNetCls/nnunetv2/training/nnUNetTrainer/variants/cls/nnUNetClsTrainerDEBUG.py
@@ -1240,7 +1240,7 @@ class nnUNetClsTrainerDEBUG(nnUNetTrainer):
                                    "forward pass (where compile is triggered) already has deep supervision disabled. "
                                    "This is exactly what we need in perform_actual_validation")
 
-        predictor = nnUNetPredictor(tile_step_size=0.5, use_gaussian=True, use_mirroring=True,
+        predictor = nnUNetCLSPredictor(tile_step_size=0.5, use_gaussian=True, use_mirroring=True,
                                     perform_everything_on_device=True, device=self.device, verbose=False,
                                     verbose_preprocessing=False, allow_tqdm=False)
         predictor.manual_initialization(self.network, self.plans_manager, self.configuration_manager, None,
